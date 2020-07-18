9a3609d65cf1101b9d2300b3454bbf965f36cf49
Kirill Rakhman <kirill.rakhman@gmail.com>
2020-07-18 14:40:28 +0200

    add link to presets

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
     <div class="container">
+        <h2 class="is-size-4">Variants</h2>
+
+        <div clasS="mb-4">
+            <a href="/?digits=__5___6__+_4_6___2_+3___7___5+_2___6___+__1___3__+___4___6_+5___3___1+_9___2_4_+__8___5__">Diagonals</a>
+        </div>
+
+        <h2 class="is-size-4">Grid</h2>
+
         <div>
             <Grid/>
         </div>
